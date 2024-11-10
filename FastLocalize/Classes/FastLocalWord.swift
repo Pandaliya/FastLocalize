621bbfee6c695c2bd75b1aca70e33c4c46ac66da
zhangpan <zhangpan@cls.cn>
2024-11-10 22:10:30 +0800

    local

---
+++ FastLocalize/Classes/FastLocalWord.swift
@@ -0,0 +1,12 @@
+//
+//  FastLocalWord.swift
+//  FastLocalize
+//
+//  Created by pan zhang on 2024/11/10.
+//
+
+import Foundation
+
+public enum FastLocalWord: String, CaseIterable {
+    case Confirm = "Confirm"
+}
